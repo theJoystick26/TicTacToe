31b951cb3ea061835cb351019855fb4692b0a455
theJoystick26 <adinjj@gmail.com>
2022-08-08 21:26:30 -0400

    Added moves

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
     
     var body: some View {
         GeometryReader { geometry in
@@ -20,10 +23,16 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 25)
                                 .foregroundColor(.blue).opacity(0.7)
                                 .frame(width: geometry.size.width / 3 - 15, height: geometry.size.width / 3 - 15)
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 50, height: 50)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if !isSquareOccupied(forIndex: i){
+                                moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                                isHumanTurn.toggle()
+                            }
                         }
                     }
                 }
@@ -31,6 +40,10 @@ struct ContentView: View {
             }
             .padding()
         }
+    }
+    
+    func isSquareOccupied(forIndex index: Int) -> Bool {
+        return moves[index] != nil
     }
 }
 
